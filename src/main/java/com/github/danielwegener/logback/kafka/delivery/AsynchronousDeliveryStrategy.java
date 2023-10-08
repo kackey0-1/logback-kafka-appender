70ccf2224cfcaeb32854a3b708034f50dc0aa768
kackey0-1 <kentaro.a.kakimoto@gmail.com>
2023-10-08 09:14:20 +0900

    migrate maven to gradle

--- src/main/java/com/github/danielwegener/logback/kafka/delivery/AsynchronousDeliveryStrategy.java
+++ src/main/java/com/github/danielwegener/logback/kafka/delivery/AsynchronousDeliveryStrategy.java
@@ -1,10 +1,7 @@
 package com.github.danielwegener.logback.kafka.delivery;
 
-import org.apache.kafka.clients.producer.BufferExhaustedException;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.errors.TimeoutException;
 
 /**
@@ -16,19 +13,15 @@ public class AsynchronousDeliveryStrategy implements DeliveryStrategy {
     public <K, V, E> boolean send(Producer<K, V> producer, ProducerRecord<K, V> record, final E event,
                                   final FailedDeliveryCallback<E> failedDeliveryCallback) {
         try {
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata metadata, Exception exception) {
-                    if (exception != null) {
-                        failedDeliveryCallback.onFailedDelivery(event, exception);
-                    }
+            producer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    failedDeliveryCallback.onFailedDelivery(event, exception);
                 }
             });
             return true;
-        } catch (BufferExhaustedException | TimeoutException e) {
+        } catch (TimeoutException e) {
             failedDeliveryCallback.onFailedDelivery(event, e);
             return false;
         }
     }
-
 }
